8e82a8d353408a0eeb2d285c14a104b9cb149b94
rlchu <ryan.chupick@gmail.com>
2021-12-02 01:13:21 -0500

    day02 pt 1

--- day02.rb
+++ day02.rb
@@ -1,14 +1,17 @@
 module Day02
   class << self
     def multiply_dimensions(input)
-      convert_to_dimensions(input)
-      input
+      dimensions = convert_to_dimensions(input)
+      horizontal, depth = dimensions[0], dimensions[1]
+      horizontal = horizontal.map{ |x| x.last.to_i }.sum
+      depth = depth.map { |x| x.first == 'down' ? x.last.to_i : -x.last.to_i }.sum
+      horizontal * depth
     end
 
     def convert_to_dimensions(input)
-      input.map do |direction|
-        direction.split(' ')
-      end
+      input.map { |direction| direction.split(' ') }
+           .group_by { |dimension| dimension.first == 'forward' }
+           .values
     end
   end
 end
