7fd15734906ad7ccd4746d7399d0e5c405484f25
rlchu <ryan.chupick@gmail.com>
2021-12-01 20:08:55 -0500

    day 1 aoc

---
+++ day01.rb
@@ -0,0 +1,21 @@
+module Day01
+  def self.count_depth_measurement_increases(input)
+    last = input.first
+    increase_count = 0
+    input[1..].each do |num|
+      num > last && increase_count += 1
+      last = num
+    end
+    increase_count
+  end
+
+  def self.count_measurement_sum_group_increases(input)
+    input_sums = input.each_cons(3).map{|batch| batch.sum}
+    count_depth_measurement_increases(input_sums)
+  end
+end
+
+input = File.readlines('input').map{ |num| num.strip.to_i }
+
+puts Day01.count_depth_measurement_increases(input)
+puts Day01.count_measurement_sum_group_increases(input)
